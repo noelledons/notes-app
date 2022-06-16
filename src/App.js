b5ab968befd635f2a4f4e922884c77767ea32491
noelledons <noelledonkor@gmail.com>
2022-06-16 19:06:06 +0100

    small amendments

--- src/App.js
+++ src/App.js
@@ -8,23 +8,18 @@ const App = () => {
 	const [notes, setNotes] = useState([
 		{
 			id: nanoid(),
-			text: 'This is my first note!',
-			date: '15/04/2021',
+			text: 'First Note!',
+			date: '13/06/2022',
 		},
 		{
 			id: nanoid(),
-			text: 'This is my second note!',
-			date: '21/04/2021',
+			text: 'Second Note!',
+			date: '14/06/2022',
 		},
 		{
 			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '30/04/2021',
+			text: 'Third Note!',
+			date: '15/06/2022',
 		},
 	]);
 
